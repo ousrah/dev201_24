ca639470e2448cf15e92b7e73d436e6c47eef496
rahmouni oussama <ousrah@hotmail.com>
2024-05-15 10:23:53 +0100

    relations

---
+++ 3 - Developpement Back end/articles/app/Models/Phone.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Phone extends Model
+{
+    use HasFactory;
+
+    protected $table = "phones";
+
+    protected $fillable = [
+        'phone_number',
+        'user_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ 3 - Developpement Back end/articles/app/Models/Post.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    protected $table = "posts";
+
+    protected $fillable = [
+        'title',
+        'description',
+        'image',
+        'user_id'
+    ];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function comments()
+    {
+        return $this->morphMany(Comment::class, 'commentable');
+    }
+}
